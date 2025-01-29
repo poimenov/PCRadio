3d89f9467c145d52a163d93cb343475e90cda7bb
poimenov <poimenov@gmail.com>
2025-01-29 11:09:54 +0200

    small fix

--- src/Extensions/CountryExtensions.cs
+++ src/Extensions/CountryExtensions.cs
@@ -1,13 +1,10 @@
 using Microsoft.AspNetCore.Components;
 using _models = PCRadio.DataAccess.Models;
-using PSC.CSharp.Library.CountryData;
 
 namespace PCRadio.Extensions;
 
 public static class CountryExtensions
 {
-    private static CountryHelper countryHelper = new CountryHelper();
-
     public static MarkupString GetCountryIcon(this _models.Country country, bool withTitle = true)
     {
         return AppCultures.GetCountrySvgMarkupString(country.Id, withTitle);
--- src/Extensions/StationExtensions.cs
+++ src/Extensions/StationExtensions.cs
@@ -1,13 +1,10 @@
 using Microsoft.AspNetCore.Components;
 using PCRadio.DataAccess.Models;
-using PSC.CSharp.Library.CountryData;
 
 namespace PCRadio.Extensions;
 
 public static class StationExtensions
 {
-	private static CountryHelper countryHelper = new CountryHelper();
-
 	public static MarkupString GetCountryIcon(this Station station)
 	{
 		return AppCultures.GetCountrySvgMarkupString(station.CountryId);
